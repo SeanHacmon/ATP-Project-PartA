89a0309ff182d9ff575c230001f02a3501ae6c02
seanhakmon <hacmons@post.bgu.ac.il>
2022-04-19 13:37:16 +0300

    Add MyMazeGenerator a randomized Prim algorithm, Finished.

--- src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -21,7 +21,69 @@ public class MyMazeGenerator extends AMazeGenerator
         int beginCol = ran2.nextInt(col);
         Pair startPoint = new Pair(beginRow, beginCol);
         Visited.add(startPoint);
+        AddNewWalls(onlyWalls, beginRow, beginCol, wallSet);
 
+        // the Prim algorithm
+        while (!wallSet.isEmpty() || onlyWalls.maze[onlyWalls.size_row -1][onlyWalls.size_col - 1] == 1)
+        {
+            Random ran = new Random();
+            int index = ran.nextInt(wallSet.size());
+            Pair p = wallSet.get(index);
+            Visited.add(p);
+            WallTooPassage(p, onlyWalls);
+            AddNewWalls(onlyWalls, (int)p.getFirst(), (int)p.getSecond(), wallSet);
+        }
+
+        return onlyWalls;
+    }
+
+
+
+
+
+    // Checking we are not out of bounds of the maze.
+    public static boolean CheckBounds(Maze m, int cellR, int cellC)
+    {
+        if (0 <= cellC && cellC < m.maze.length && 0 < cellR && cellR < m.maze[0].length)
+            return true;
+        return false;
+    }
+
+
+    // Creates a Maze of 100% walls.
+    public static Maze FullOfWalls(int row, int col)
+    {
+        Maze onlyWalls = new Maze(row, col);
+        for (int i = 0; i < row; i++) 
+        {
+            for (int j = 0; j < col; j++) 
+            {
+                onlyWalls.maze[i][j] = 1;
+            }   
+        }
+        return onlyWalls;
+    }
+
+    // Receive a pair and a maze and turns the specific cell from 0 to 1.
+    public static void WallTooPassage(Pair p, Maze m)
+    {
+        for (int i = 0; i < m.maze.length; i++)
+        {
+            for (int j = 0; j < m.maze[0].length; j++)
+            {
+                if (p.getFirst().equals(i) && p.getSecond().equals(j))
+                {
+                    m.maze[i][j] = 1;
+                    return;
+                }
+            }
+
+        }
+    }
+
+    // Adding new walls to the WallSet by a specific cell getting his neighbors.
+    public static void AddNewWalls(Maze onlyWalls, int beginRow, int beginCol, List<Pair> wallSet)
+    {
         // adds the neighbor below if it is in bound.
         if (CheckBounds(onlyWalls, beginRow+1, beginCol))
         {
@@ -49,50 +111,8 @@ public class MyMazeGenerator extends AMazeGenerator
             Pair p = new Pair(beginRow, beginCol-1);
             wallSet.add(p);
         }
-
-        while (!wallSet.isEmpty())
-        {
-            Random ran = new Random();
-            int index = ran.nextInt(wallSet.size());
-            Pair p = wallSet.get(index);
-            //// TODO need to continue the algorithm
-        }
-
-        
-        
-        
-        
-        
-        
-        return onlyWalls;
-    }
-
-
-
-
-
-    // Checking we are not out of bounds of the maze.
-    public static boolean CheckBounds(Maze m, int cellR, int cellC)
-    {
-        if (0 <= cellC && cellC < m.maze.length && 0 < cellR && cellR < m.maze[0].length)
-            return true;
-        return false;
-    }
-
-
-
-    // Creates a Maze of 100% walls.
-    public static Maze FullOfWalls(int row, int col)
-    {
-        Maze onlyWalls = new Maze(row, col);
-        for (int i = 0; i < row; i++) 
-        {
-            for (int j = 0; j < col; j++) 
-            {
-                onlyWalls.maze[i][j] = 1;
-            }   
-        }
-        return onlyWalls;
     }
 
 }
+
+
