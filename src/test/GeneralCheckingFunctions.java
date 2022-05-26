29bd6753e03208291ee2643fcb2187f6e46fed9c
seanhakmon <hacmons@post.bgu.ac.il>
2022-05-26 18:02:33 +0300

    Part A- - Finished Part A.

---
+++ src/test/GeneralCheckingFunctions.java
@@ -0,0 +1,9 @@
+package test;
+
+public class GeneralCheckingFunctions
+{
+    public static String getGithubLink(){
+//change the <username> in the link to the username of the student who created the github project:
+        return "https://github.com/SeanHacmon/Maze_Project";
+    }
+}
